5a2245d4026ccaff4b6934607e399ee17e37f20f
vaishnavi <58027647+imvaishu@users.noreply.github.com>
2020-01-19 17:10:56 +0530

    added game class

---
+++ game.js
@@ -0,0 +1,7 @@
+class Game {
+  constructor(snake, ghostSnake, food) {
+    this.snake = snake;
+    this.ghostSnake = ghostSnake;
+    this.food = food;
+  }
+}
--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const NUM_OF_ROWS = 60;
 const GRID_ID = 'grid';
 
 const getGrid = () => document.getElementById(GRID_ID);
-const getCellId = (colId, rowId) => colId + '_' + rowId;
+const getCellId = (colId, rowId) => `${colId}_${rowId}`;
 
 const getCell = (colId, rowId) =>
   document.getElementById(getCellId(colId, rowId));
@@ -86,22 +86,23 @@ const main = function () {
   const snake = snakeInit();
   const ghostSnake = ghostSnakeInit();
   const food = new Food(5, 5);
+  const game = new Game(snake, ghostSnake, food);
 
-  attachEventListeners(snake);
+  attachEventListeners(game.snake);
   createGrids();
-  drawSnake(snake);
-  drawSnake(ghostSnake);
-  drawFood(food);
+  drawSnake(game.snake);
+  drawSnake(game.ghostSnake);
+  drawFood(game.food);
 
   setInterval(() => {
-    moveAndDrawSnake(snake);
-    moveAndDrawSnake(ghostSnake);
+    moveAndDrawSnake(game.snake);
+    moveAndDrawSnake(game.ghostSnake);
   }, 200);
 
   setInterval(() => {
     let x = Math.random() * 100;
     if (x > 50) {
-      ghostSnake.turnLeft();
+      game.ghostSnake.turnLeft();
     }
   }, 500);
 };
